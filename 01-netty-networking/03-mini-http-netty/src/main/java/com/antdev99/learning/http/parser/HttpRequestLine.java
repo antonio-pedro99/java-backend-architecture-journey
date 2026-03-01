deefae1cdcc5fc5d1bc38394e5292afb48e57ad9
Antonio Pedro <tonio.pedro99@gmail.com>
2026-03-01 14:26:23 +0000

    refactor: rename HttpTopLine to HttpRequestFirstLine and update related classes

--- 01-netty-networking/03-mini-http-netty/src/main/java/com/antdev99/learning/http/parser/HttpRequestLine.java
+++ 01-netty-networking/03-mini-http-netty/src/main/java/com/antdev99/learning/http/parser/HttpRequestLine.java
@@ -1,11 +1,8 @@
 package com.antdev99.learning.http.parser;
 
 /**
- * A simple representation of an HTTP request line, which consists of the HTTP method, the requested path, and the HTTP version.
- * @param method the HTTP method (e.g., GET, POST, PUT, DELETE)
- * @param path the requested path (e.g., /index.html)
- * @param version the HTTP version (e.g., HTTP/1.1)
+ * Marker interface for each line on an HTTP request
  */
-public record HttpRequestLine(String method, String path, String version)
-        implements HttpTopLine {
+public interface HttpRequestLine {
+    // NO-OP
 }
